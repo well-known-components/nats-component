350a22bed2219c6875c8c4aa23337987e684b4e8
Hugo Arregui <hugo@decentraland.org>
2022-08-29 14:02:44 -0300

    addressing review comments

--- src/test-component.ts
+++ src/test-component.ts
@@ -1,17 +1,17 @@
 import { IBaseComponent } from "@well-known-components/interfaces"
 import mitt from "mitt"
-import { INatsComponent, Subscription, NatsEvents, NatsMsg, SubscriptionCallback } from "./types"
+import { INatsComponent, Subscription, NatsEvents, SubscriptionCallback } from "./types"
 
 /**
  * Create a local NATS component, for testing purposes
  * @public
  */
 export async function createLocalNatsComponent(): Promise<INatsComponent & IBaseComponent> {
-  const callbacks = new Map<string, SubscriptionCallback>()
+  const callbacks = new Map<string, Set<SubscriptionCallback>>()
   const events = mitt<NatsEvents>()
 
   function publish(topic: string, data: Uint8Array): void {
-    callbacks.forEach((cb, pattern) => {
+    callbacks.forEach((cbs, pattern) => {
       const sPattern = pattern.split(".")
       const sTopic = topic.split(".")
 
@@ -25,12 +25,16 @@ export async function createLocalNatsComponent(): Promise<INatsComponent & IBase
         }
       }
 
-      cb(null, { subject: topic, data })
+      for (const cb of cbs) {
+        cb(null, { subject: topic, data })
+      }
     })
   }
 
   function subscribe(pattern: string, cb: SubscriptionCallback): Subscription {
-    callbacks.set(pattern, cb)
+    const cbs = callbacks.get(pattern) || new Set<SubscriptionCallback>()
+    cbs.add(cb)
+    callbacks.set(pattern, cbs)
     return {
       unsubscribe: () => false,
     }
